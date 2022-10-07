cf5a0bf8f8e5dc7499842363c0c8d23e2f430224
Jack Butler <jack.butler19@protonmail.com>
2022-10-07 12:34:44 -0400

    Play a round of rock, paper, scissors.
    Function takes imput from getComputerSelection and
    player's selection, compares them to each other using the
    game rules, and then selects and declares a winner. If both
    choose the same thing, game results in a draw. Next will be a function
    to play through a game of multiple rounds.

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function getComputerChoice(choices) {
     // console.log(computerChoice);
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    cs = computerSelection;
+    ps = playerSelection[0].toUpperCase() + playerSelection.substring(1,playerSelection.length + 1).toLowerCase();
+    if (cs === ps) {
+        return "It's a draw! You both chose " + cs + ".";
+    } else if (ps === "Rock" && cs === "Scissors") {
+        return "You win! " + ps + " beats " + cs + ".";
+    } else if (ps === "Paper" && cs === "Rock") {
+        return "You win! " + ps + " beats " + cs + ".";
+    } else if (ps === "Scissors" && cs === "Paper") {
+        return "You win! " + ps + " beats " + cs + ".";
+    } else {
+        return "You lose! " + cs + " beats " + ps + ".";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(choices);
+console.log(playRound(playerSelection, computerSelection));
